d1f5726883afa67f7f84526a392997e0df9b539d
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-10-29 15:17:51 -0400

    added better noise generation

--- Landmass Generation/Program.cs
+++ Landmass Generation/Program.cs
@@ -6,7 +6,7 @@ namespace terrain.gen
     class MainClass
     {
         static List<double> seeds, heightMaps;
-        static int length = 200;
+        static int length = 300;
 
         static ImprovedNoise noise;
 
@@ -18,31 +18,37 @@ namespace terrain.gen
             heightMaps = new List<double>();
 
             double finalHeight = 0;
-            double amplitude = 10.8;
+            double lacunarity = 2;
+            double persistance = .5;
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 25; i++)
             {
                 seeds.Add(new Random().Next(1, 10000000));
             }
-
-            double frequency = 90; // size of the terrain generation
+            Console.WriteLine(seeds[0]);
 
             for (double x = 0; x < length; x++) {
                 for (double z = 0; z < length; z++) {
 
-                    int c = 1;
-                    foreach (int seed in seeds)
-                        finalHeight += noise.noise(x/length* (frequency/(2*c)), z/length* (frequency/(2*c)), seed)*(amplitude+(.25*c));
-                        c++;
+                    double frequency = 1;
+                    double amplitude = 1;
+
+                    int c = 0;
+                    for (int oct = 0; oct < 10; oct++) {
+
+                        double n = noise.noise(x/20 * frequency, z/20 * frequency, seeds[0]) * 2 - 1;
+                        finalHeight += (n * amplitude*40)+15;
+
+                        frequency *= Math.Pow(lacunarity, oct);
+                        amplitude *= Math.Pow(persistance, oct);
+                    }
 
                     heightMaps.Add(finalHeight);
-                    Console.WriteLine(heightMaps.Count);
-
                     finalHeight = 0;
                 }
             }
 
-            new Game(heightMaps, 1000, 720);
+            new terrain.gen.GameModules.Game(heightMaps, 1000, 720);
         }
     }
 }
